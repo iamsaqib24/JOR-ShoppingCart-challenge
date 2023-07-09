13d730cd038d4ec6d9db4cba4a76d78bde2797e0
Saqib <96372222+iamsaqib24@users.noreply.github.com>
2023-07-09 19:12:21 +0530

    Initial commit

---
+++ src/CartTable.js
@@ -0,0 +1,43 @@
+function CartTable({ items }) {
+  // TODO: Map through “items”, creating 1 row
+  // per item.
+
+  return (
+    <table className="shopping-cart">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Title</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(
+      ({imageSrc,
+        imageAlt,
+        title,
+        price,
+       id}) => (
+        <tr key={id} className="cart-row">
+          <td>
+            <img
+              className="product-thumb"
+              src={imageSrc}
+              alt={imageAlt}
+            />
+          </td>
+          <td>
+            {title}
+          </td>
+          <td>
+            ${price}
+          </td>
+        </tr>
+        )
+        )}
+      </tbody>
+    </table>
+  );
+}
+
+export default CartTable;
